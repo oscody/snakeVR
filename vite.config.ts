c7c64ed4285cb895961231684984b53fcf8b2f3e
oscody <bogleoneil@gmail.com>
2026-05-25 14:05:05 -0400

    chore: update project configuration and dependencies - Add "gh-pages" as a dev dependency in package.json and package-lock.json. - Update base path in vite.config.ts to '/snakeVR/' for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
+  base: '/snakeVR/',
+
   plugins: [
     mkcert(),
     iwsdkDev({
@@ -32,5 +34,4 @@ export default defineConfig({
     esbuildOptions: { target: "esnext" },
   },
   publicDir: "public",
-  base: "./",
 });
